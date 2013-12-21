ce946cc229bd626f8c0a567946f0857646322099
Sean Donovan <swordrune10@gmail.com>
2013-12-21 15:33:24 -0700

    Add dtb inline building

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,13 +57,17 @@ TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 TARGET_SIMULATOR := false
 
+BOARD_CUSTOM_BOOTIMG_MK := device/lge/falcon_umts/mkbootimg.mk
+
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/motorola/falcon_umts
 TARGET_KERNEL_CONFIG := msm8226_mmi_defconfig
+
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags androidboot.write_protect=0 zcache androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --dt $(LOCAL_PATH)/dt.img
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 # Wifi related defines
 BOARD_HAS_QCOM_WLAN := true
